3f051b9dde849d398f64dd04138ce5fa1536df2b
Ben Lavender <bhuga@github.com>
2012-10-10 14:07:11 -0500

    Initial Commit

---
+++ blackmagic audio sync issue demonstration/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  blackmagic audio sync issue demonstration
+//
+//  Created by Ben Lavender on 10/10/12.
+//  Copyright (c) 2012 Ben Lavender. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
